dcbc3636a474e5a646b28ee8f8f9495ec90bf3ea
Barbara Pušnar <barbara.pusnar@scng.si>
2022-10-13 10:59:07 +0200

    objekti

---
+++ NiSignala/NiSignala/Kupec.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiSignala
+{
+    internal class Kupec
+    {
+        protected string ime;
+        protected decimal stanje;
+
+        public string Ime { get => ime; set => ime = value; }
+        public decimal Stanje { get => stanje;}
+        //ne moreš spreminjati izven Kupec
+        public void BeležiPlačilo(decimal plačilo)
+        {
+            stanje -= plačilo;
+        }
+        public virtual void BeležiKlic(int min, TipKlica tip)
+        {
+            switch (tip)
+            {
+                case TipKlica.Stacionarno: 
+                    stanje += min * 0.12m;
+                    break;
+                case TipKlica.Mobilno:
+                    stanje += min * 0.03m;
+                    break;
+                default:
+                    break;
+            }
+        }
+        public override string ToString()
+        {
+            return ime + " dolguje " + stanje + " evrov";
+        }
+
+    }
+}
---
+++ NiSignala/NiSignala/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiSignala
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kupec janez = new Kupec();
+            janez.Ime = "Janez Novak";
+            janez.BeležiKlic(10, TipKlica.Stacionarno);
+            Console.WriteLine(janez.Ime+" dolguje "+janez.Stanje);
+            Console.WriteLine("get type "+janez.GetType());
+            Console.WriteLine("to string " + janez.ToString());
+            Kupec alenka = new Kupec60();
+            alenka.Ime = "Alenka Novak";
+            alenka.BeležiKlic(100, TipKlica.Mobilno);
+            Console.WriteLine(alenka.Ime + " dolguje " + alenka.Stanje);
+            Console.WriteLine("get type " + alenka.GetType());
+            Console.WriteLine("to string " + alenka.ToString());
+            Console.ReadLine();
+        }
+    }
+}
